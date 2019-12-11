bda8b1809ea6e056f00fc65b157c201208a141f7
KaishuaiXu <xu.kaishuai@gmail.com>
2019-12-11 15:09:56 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,32 @@
+# Improved Quantile Regression Neural Network
+
+This package is based on the paper, [An **improved quantile regression neural network** for probabilistic load forecasting](https://ieeexplore.ieee.org/abstract/document/8419220/), [W Zhang](https://scholar.google.com/citations?user=aanT6TIAAAAJ&hl=en&oi=sra).
+
+## Usage
+
+```python
+from iqrnn import get_model
+import numpy as np
+
+# Generate the synthetic data
+x1 = np.sin(np.arange(0, 9, 0.01))
+x2 = np.cos(np.arange(0, 9, 0.01))
+x3 = x1**2
+x4 = (x1+x2)/2
+
+Xtrain = np.vstack((x2, x3, x4)).T
+Ytrain = np.array([x1]*99).T
+
+# Parameters
+input_dim = 3
+num_hidden_layers = 1
+num_units = [200]
+act = ['relu']
+gauss_std = 0.3
+
+# Get model
+model = get_model(input_dim, num_units, act, gauss_std, num_hidden_layers)
+
+# Train
+model.fit(x=Xtrain, y=Ytrain, epochs=10)
+```
--- iqrnn.py
+++ iqrnn.py
@@ -1,1 +1,24 @@
 import numpy as np
+from keras import backend as K
+from keras.layers import Input, Dense, GaussianNoise
+from keras.models import Model
+from keras import regularizers
+
+def qloss(y_true, y_pred):
+    q = np.array(range(1, 100))
+    tmp1 = (q / 100 - 1) * (y_true - y_pred)
+    tmp2 = q / 100 * (y_true - y_pred)
+    return K.mean(K.maximum(tmp1, tmp2))
+
+def get_model(input_dim, num_units, act, gauss_std=0.3, num_hidden_layers=1):
+    input = Input((input_dim,), name='input')
+
+    for i in range(num_hidden_layers):
+        x = Dense(num_units[i], use_bias=True, kernel_initializer='he_normal', bias_initializer='he_normal', 
+                kernel_regularizer=regularizers.l2(0.001), activation=act[i])(input)
+        x = GaussianNoise(gauss_std)(x)
+    x = Dense(99, activation=None, use_bias=True, kernel_initializer='he_normal', bias_initializer='he_normal')(x)
+
+    model = Model(inputs=input, outputs=x)
+    model.compile(loss=qloss, optimizer='adam', metrics=['accuracy'])
+    return model
