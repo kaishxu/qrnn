fbe0088bb8ff4891593cc7e97f332fe0ab698c74
KaishuaiXu <xu.kaishuai@gmail.com>
2019-12-27 22:29:01 +0800

    update

--- qrnn.py
+++ qrnn.py
@@ -18,7 +18,7 @@ def get_model(input_dim, num_units, act, gauss_std=0.3, num_hidden_layers=1):
     for i in range(num_hidden_layers):
         x = Dense(num_units[i], use_bias=True, kernel_initializer='he_normal', bias_initializer='he_normal', 
                 kernel_regularizer=regularizers.l2(0.001), activation=act[i])(x)
-        x = GaussianNoise(gauss_std)(x)
+        x = GaussianNoise(gauss_std)(x)  #似乎不适用与小模型?
     x = Dense(99, activation=None, use_bias=True, kernel_initializer='he_normal', bias_initializer='he_normal')(x)
 
     model = Model(inputs=input_, outputs=x)
